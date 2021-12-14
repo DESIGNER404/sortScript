bc91272770bad94da19cd4dfd1939c3c0a30b9ca
Alexandr Makarov <Designer.404@yandex.ru>
2021-12-14 16:40:39 +0300

    upgrade

--- sortScript.py
+++ sortScript.py
@@ -9,12 +9,16 @@ two = "\033[32m"
 extension = ['png', 'jpg', 'svg']
 
 path = "/Users/rosinnovacii/Desktop"
+path2 =f"{path}/Delete1"
+os.mkdir(path2)
 
 file_name = os.listdir(path)
 for m in file_name:
     fileSplit = m.split('.')[-1]
     if fileSplit in extension:
-        shutil.move(f"/Users/rosinnovacii/Desktop/{m}", f"/Users/rosinnovacii/Desktop/Delete/{m}")
+        shutil.move(f"{path}/{m}", f"{path}/Delete/{m}")
         print (one + f"есть такой фаил: {m}")
     else:
         print (two + f"нет такого файла: {m}")
+
+os.rmdir(path2)
