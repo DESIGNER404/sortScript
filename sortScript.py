dcac2fdf7f5b3e7b61effe3896184a02756f843f
Alexandr Makarov <Designer.404@yandex.ru>
2021-12-21 13:14:26 +0300

    numerate

--- sortScript.py
+++ sortScript.py
@@ -4,6 +4,10 @@ import subprocess
 import time
 from datetime import date, datetime
 
+def getNum(path:str):
+    d = max(path.split(sep="--")[-1])
+    return d
+
 Red = "\033[31m"
 Green = "\033[32m"
 
@@ -11,7 +15,8 @@ extension = ['png', 'jpg', 'svg', 'blend1', 'fbx']
 
 path = "/Users/rosinnovacii/Desktop"
 path2 =f"{path}/Delete1"
-os.mkdir(path2)
+if not os.path.exists(path2):
+    os.mkdir(path2)
 
 file_name = os.listdir(path)
 for m in file_name:
@@ -28,7 +33,17 @@ question = input("Хотите удалите все файлы?(y/n):")
 if question == "y":
     shutil.rmtree(path2)
 else:
-   shutil.move(path2, f"{path}/Delete/Delete_{current_date}")
+   in_delete = os.listdir("/Users/rosinnovacii/Desktop/Delete")
+   def getMax(dir_list):
+        nums = []
+        for j in dir_list:
+            parts = j.split("--")
+            if len(parts) > 1:
+                nums.append(int(parts[-1]))
+        return max(nums)
+   maxNum = getMax(in_delete)
+   ret = maxNum + 1
+   shutil.move(path2, f"{path}/Delete/Delete_{current_date}--{ret}")
 
 print("\n\nDone!")
 input()
